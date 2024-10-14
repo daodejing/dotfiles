c52c34c71ffe33781eceec177c3f6984590dfc26
Nick <mills-nick@gmo.jp>
2024-10-14 19:00:53 +0900

    remove ensure_installed from treesitter

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -116,7 +116,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "rust" },
+				-- ensure_installed = { "c", "lua", "rust" },
 				highlight = { enable = true },
 			})
 		end,
