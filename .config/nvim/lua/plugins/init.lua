2b446eb7869c3a82f579c161f77e40785ee1ef6e
Nick <mills-nick@gmo.jp>
2024-10-14 18:15:08 +0900

    remove bufferline plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,65 +1,66 @@
 return {
 	{
-		'rebelot/kanagawa.nvim',
+		"rebelot/kanagawa.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd { cmd = 'colorscheme', args = { 'kanagawa' } }
-		end
+			vim.cmd({ cmd = "colorscheme", args = { "kanagawa" } })
+		end,
 	},
-	'moll/vim-bbye',
+	"moll/vim-bbye",
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = "nvim-lua/plenary.nvim",
 		keys = {
-			{ "<C-t>", "<CMD>Telescope<CR>",             mode = { "n", "i", "v" } },
-			{ "<C-p>", "<CMD>Telescope find_files<CR>",  mode = { "n", "i", "v" } },
-			{ "<C-l>", "<CMD>Telescope live_grep<CR>",   mode = { "n", "i", "v" } },
-			{ "<C-c>", "<CMD>Telescope commands<CR>",    mode = { "n", "i", "v" } },
-			{ "<C-k>", "<CMD>Telescope keymaps<CR>",     mode = { "n", "i", "v" } },
+			{ "<C-t>", "<CMD>Telescope<CR>", mode = { "n", "i", "v" } },
+			{ "<C-p>", "<CMD>Telescope find_files<CR>", mode = { "n", "i", "v" } },
+			{ "<C-l>", "<CMD>Telescope live_grep<CR>", mode = { "n", "i", "v" } },
+			{ "<C-c>", "<CMD>Telescope commands<CR>", mode = { "n", "i", "v" } },
+			{ "<C-k>", "<CMD>Telescope keymaps<CR>", mode = { "n", "i", "v" } },
 			{ "<C-s>", "<CMD>Telescope grep_string<CR>", mode = { "n", "i", "v" } },
 		},
-		config = true
+		config = true,
 	},
-	'dhruvmanila/telescope-bookmarks.nvim',
-	'benfowler/telescope-luasnip.nvim',
-	'nvim-telescope/telescope-live-grep-args.nvim',
-	'princejoogie/dir-telescope.nvim',
+	"dhruvmanila/telescope-bookmarks.nvim",
+	"benfowler/telescope-luasnip.nvim",
+	"nvim-telescope/telescope-live-grep-args.nvim",
+	"princejoogie/dir-telescope.nvim",
 	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
 	},
-	'lewis6991/gitsigns.nvim',
-	'p00f/nvim-ts-rainbow',
-	'nvim-treesitter/playground',
+	"lewis6991/gitsigns.nvim",
+	"p00f/nvim-ts-rainbow",
+	"nvim-treesitter/playground",
 	{
-		'ggandor/leap.nvim',
+		"ggandor/leap.nvim",
 		config = function()
-			local leap = require('leap')
+			local leap = require("leap")
 			leap.add_default_mappings()
 			leap.setup({})
-		end
+		end,
 	},
-	'tpope/vim-surround',
-	'tpope/vim-fugitive',
-	'tpope/vim-sensible',
-	'tpope/vim-unimpaired',
-	'tpope/vim-repeat',
-	'tpope/vim-speeddating',
-	'tpope/vim-obsession',
-	'christoomey/vim-tmux-navigator',
+	"tpope/vim-surround",
+	"tpope/vim-fugitive",
+	"tpope/vim-sensible",
+	"tpope/vim-unimpaired",
+	"tpope/vim-repeat",
+	"tpope/vim-speeddating",
+	"tpope/vim-obsession",
+	"christoomey/vim-tmux-navigator",
 	{
-		'windwp/nvim-autopairs',
+		"windwp/nvim-autopairs",
 		config = function()
-			require('nvim-autopairs').setup()
-		end
+			require("nvim-autopairs").setup()
+		end,
 	},
 	{
-		'akinsho/bufferline.nvim',
+		"akinsho/bufferline.nvim",
+		enabled = false,
 		config = function()
-			require('bufferline').setup()
-		end
+			require("bufferline").setup()
+		end,
 	},
 	{
 		"folke/which-key.nvim",
@@ -74,21 +75,21 @@ return {
 		end,
 	},
 	{
-		'nvim-neo-tree/neo-tree.nvim',
+		"nvim-neo-tree/neo-tree.nvim",
 		keys = {
-			{ '<leader>ft', '<cmd>Neotree toggle<cr>', desc = 'NeoTree' }
+			{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
 		},
 		dependencies = {
-			'MunifTanjim/nui.nvim',
-			'nvim-lua/plenary.nvim',
-			'nvim-tree/nvim-web-devicons',
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-			require('neo-tree').setup()
+			require("neo-tree").setup()
 		end,
 	},
 	{
-		'VonHeikemen/lsp-zero.nvim',
+		"VonHeikemen/lsp-zero.nvim",
 		dependencies = {
 			"neovim/nvim-lspconfig",
 			"williamboman/mason.nvim",
@@ -111,17 +112,17 @@ return {
 			end)
 			lsp.nvim_workspace()
 			lsp.setup()
-			vim.diagnostic.config { virtual_text = true }
-		end
+			vim.diagnostic.config({ virtual_text = true })
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup {
+			require("nvim-treesitter.configs").setup({
 				ensure_installed = { "c", "lua", "rust" },
-				highlight = { enable = true, }
-			}
-		end
+				highlight = { enable = true },
+			})
+		end,
 	},
 }
